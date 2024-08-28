f2ed61c1fe29b7c4ca1d4e46474435025531cead
Mujahid Niaz <mujahid.niaz@sonova.com>
2024-08-28 03:09:52 +0200

    fixed run_linux issue

--- run_mac.sh
+++ run_mac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Create the necessary directories if they don't exist
 mkdir -p app_data
 mkdir -p data
 docker compose down
--- run_windows.bat
+++ run_windows.bat
@@ -1,9 +1,6 @@
 @echo off
-
-REM Create the necessary directories if they don't exist
 if not exist app_data mkdir app_data
 if not exist data mkdir data
 docker pull mujahid002/sherlock-ai-frontend-groq
 docker pull mujahid002/sherlock-ai-backend-groq
-REM Run Docker Compose with the --build flag
 docker-compose up --build -d
