6341f8e3f725c9865ef19ffca96be074b235d973
Mujahid Niaz <mujahid.niaz@sonova.com>
2024-08-31 04:04:50 +0200

    fixed run_linux issue

--- run_linux.sh
+++ run_linux.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Create the necessary directories if they don't exist
 docker compose down
 mkdir -p app_data
 mkdir -p data
 docker pull mujahid002/sherlock-ai-frontend-groq
 docker pull mujahid002/sherlock-ai-backend-groq
-# Run Docker Compose with the --build flag
 docker compose up -d
